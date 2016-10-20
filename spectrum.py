1e2518636e89a46813de908b77e0a9967ba573dd
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-10-20 14:55:48 -0700

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -3,8 +3,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 matplotlib.use('tkagg')
+plt.ion()
 
-def plot_spectrum(images, region, average):
+def plot_spectrum(images, rg, average):
 
   adc_counts = 0
   
@@ -14,17 +15,21 @@ def plot_spectrum(images, region, average):
     
    
     imarray = imtools.ImGrid(im)
-    adc_counts += imtools.find_spectrum(imarray)
+    adc_counts += imtools.find_spectrum(imarray, region=rg)
     
   plt.figure(1)
-  plt.hist(adc_counts, )
+  plt.hist(np.arange(counts), weights=adc_counts)
+  plt.xlabel('ADC Counts')
+  plt.ylabel('Frequency')
+  plt.show()
     
 
 if __name__ == '__main__':
   from argparse import ArgumentParser
   parser = ArgumentParser(description='')
   parser.add_argument("--in", dest='images', required=True, nargs='+', help='')
-  parser.add_argument("--region,
+  parser.add_argument("--region, help='')
   parser.add_argument("--average", help='')
   args = parser.parse_args()
+                      
   
