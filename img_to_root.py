f1fc3b74416c03e66741d6a917d66ec5204f70a6
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-10-20 02:45:33 -0700

    Update and rename jpeg_to_root.py to img_to_root.py

--- img_to_root.py
+++ img_to_root.py
@@ -3,21 +3,20 @@ import ROOT as r
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import rawpy
 import math
 import time
 from scipy.signal import convolve2d
-from hotcell import vbranch
+from imtools import vbranch
 from bg_threshold import find_bg
 
 matplotlib.use('tkagg')
 
 # given an Image object and a threshold of pixels to keep, returns L2 values
-def set_thresh(im, thresh):
+def set_thresh(imarray, thresh):
 
-    n_bands = len(im.mode)
+    n_bands = len(imarray.bands)
     thresh_array = np.repeat(-1,n_bands)
-    im_pix = im.height*im.width
+    im_pix = imarray.height*imarray.width
     target_pix = thresh*im_pix
     histarray = np.array(im.histogram()).reshape((n_bands,256))
 
@@ -30,21 +29,26 @@ def set_thresh(im, thresh):
     return thresh_array
 
 def convert_to_root(images, out, l1thresh=0, l2auto=True, l2manual=0, l2plus=0, sauto=True, smanual=0, border=0, \
-                    max_img=0, numpy=True):
+                    max_img=0, source_format=False):
 
+    raw_types = ['.dng']
+    
+    avg3_kernel = np.array([[1,1,1],[1,0,1],[1,1,1]])/8.0
+    avg5_kernel = np.array([[1,1,1,1,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,1]])/16.0
+    
+    n_images_auto = 100
+    
     saved_pix = 0
     total_pix = 0
 
     # handle S threshold settings
     if sauto:
-        n_images_auto = 100
         s_grid = find_bg(images[:n_images_auto], out.split('.')[0]+"_bg.png")
         images = images[n_images_auto:]
     elif smanual:
-        s_grid = np.array(Image.open(smanual)).transpose(1,0,2).astype(int)
+        s_grid = np.array(Image.open(smanual)).astype(int)
     else:
-        im = Image.open(images[0])
-        s_grid = np.zeros((im.width, im.height, len(im.mode)),dtype=int)
+        s_grid = 0
         
     if l2plus:
         s_grid += l2plus
@@ -56,18 +60,18 @@ def convert_to_root(images, out, l1thresh=0, l2auto=True, l2manual=0, l2plus=0, 
     t = r.TTree("events", 'TTree with JPEG data')
 
     n_img = np.array([0], dtype=int)
-    phone_time = np.array([0], dtype=long)
     pix_n = np.array([0], dtype=int)
     color = np.array('', dtype=str)
-    source = np.array([0], dtype=bool)
     l2 = np.array([0], dtype=int)
    
     t.Branch('n_img', n_img, 'n_img/i')
-    t.Branch('t', phone_time, 't/i')
     t.Branch('pix_n', pix_n, 'pix_n/i')
     t.Branch('col', color, 'col/C')
-    t.Branch('source', source, 'source/O')
     t.Branch('l2thresh', l2, 'l2thresh/i')
+    
+    if source_format:
+        source = np.array([0], dtype=bool)
+        t.Branch('source', source, 'source/O')
     
 
     vbranch(t, 'pix_x', btype=int)
@@ -78,46 +82,30 @@ def convert_to_root(images, out, l1thresh=0, l2auto=True, l2manual=0, l2plus=0, 
     vbranch(t, 'l2s', btype=int)
 
 
-    avg3_pix = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
-    avg5_pix = [(-2,-2),(-2,-1),(-2,0),(-2,1),(-2,2),(-1,-2),(-1,2),(0,-2), \
-                (0,2),(1,-2),(1,2),(2,-2),(2,-1),(2,0),(2,1),(2,2)]
-
-    avg3_kernel = np.array([[1,1,1],[1,0,1],[1,1,1]])/8.0
-    avg5_kernel = np.array([[1,1,1,1,1,1,1],[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,0,0,0,0,0,1], \
-                            [1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,1,1,1,1,1,1]])/24.0
-
-
     # fill TTree
     print "Starting loop..."
-    for i,jpg in enumerate(images):
+    for i,im_name in enumerate(images):
         
-        phone_time[0] = long(jpg[-20:-7])
+        raw = im_name.split('.')[1] in raw_types
         
         print
-        print "Image %d/%d: t = %d" % (i+1,len(images),phone_time[0])
+        print "Image %d/%d:" % (i+1,len(images))
 
-        im = Image.open(jpg)
-        mode = im.mode
-        n_bands = len(mode)
-        
-        # enforce L1 threshold
-        min_band, max_band = zip(*list(im.getextrema()))
-        if n_bands > 1 and max(max_band) < l1thresh: continue
-        elif max_band < l1thresh: continue
-        n_img[0] = i+1
+        imarray = get_imarray(im_name, israw=raw)
+        n_bands = len(imarray.bands)
 
-        im_pix = im.width*im.height
-        source[0] = bool(int(jpg[-5]))
+        im_pix = imarray.width*imarray.height
+        if source_format:
+            source[0] = bool(int(jpg[-5]))
 
         # set L2 threshold
         if l2auto:
             if sauto or smanual:
-                cx, cy = im.width/2, im.height/2
-                c_side_half = int(im.height/2/math.sqrt(2))
-                cropped = im.crop((cx-c_side_half, cy-c_side_half, cx+c_side_half, cy+c_side_half))
-                l2array = set_thresh(cropped, l2auto)
+                cx, cy = imarray.width/2, imarray.height/2
+                c_side_half = int(imarray.height/2/math.sqrt(2))
+                l2array = set_thresh(imarray[cx-c_side_half:cx+c_side_half,cy-c_side_half:cy+c_side_half], l2auto)
             else:
-                l2array = set_thresh(im, l2auto)
+                l2array = set_thresh(imarray, l2auto)
             
                 
         elif l2manual:
@@ -132,110 +120,42 @@ def convert_to_root(images, out, l1thresh=0, l2auto=True, l2manual=0, l2plus=0, 
         
             
         print "L2 threshold: \t",
-        for cval, c in enumerate(im.mode):
+        for cval, c in enumerate(imarray.bands):
             print "%s: %d" % (c, l2array[cval]),
         print
 
-        # determine relevant S thresholds
-        l1s_satisfied = not (sauto or smanual)
-        
+        # determine relevant S thresholds    
         grid_min = np.amin(np.amin(s_grid,axis=0),axis=0)
         l1diff = np.repeat(l1thresh,3)-grid_min
         l2diff = l2array-grid_min
-                
-                                
+     
+
         x_vectors = [r.vector('int')() for i in xrange(n_bands)]
         y_vectors = [r.vector('int')() for i in xrange(n_bands)]
         val_vectors = [r.vector('int')() for i in xrange(n_bands)]
         avg3_vectors = [r.vector('double')() for i in xrange(n_bands)]
         avg5_vectors = [r.vector('double')() for i in xrange(n_bands)]
         l2s_vectors = [r.vector('int')() for i in xrange(n_bands)]
-        
-        # find val, avg3, avg5
 
-        if not numpy:
-        
-            for j,val in enumerate(im.getdata()):
+        # enforce L1S
+        if np.count_nonzero(imarray>=s_grid+l1diff) == 0: continue
+            n_img[0] += 1
 
-                # enforce L2 threshold
-                if n_bands > 1 and max(val) < minl2thresh: continue
-                elif n_bands == 1 and val < minl2thresh: continue
-                elif n_bands > 1 and max(val-l2array) < 0: continue
-
-                x,y = j%im.width, j/im.width
-
-                if x < border or x >= im.width-border or y < border or \
-                   y >= im.height-border: continue
-
-                # determine L1S and L2S
-                if sauto or smanual:
-                    val_minus_l2s = val-s_grid[x,y]-l2diff
-                    if max(val_minus_l2s) < 0: continue
-                    if max(val-s_grid[x,y]-l1diff) >= 0:
-                        l1s_satisfied = True
-
-
-                # find avg3 and avg5
-                        
-                sum3 = np.zeros(n_bands)
-                sum5 = np.zeros(n_bands)
-
-                n3 = 0
-                n5 = 0
-                    
-                for dx,dy in avg3_pix:
-                    xprime = x+dx
-                    yprime = y+dy
-                    if xprime >= 0 and xprime < im.width and yprime >= 0 and yprime < im.height:
-                        n3 += 1
-                        sum3 += im.getpixel((xprime,yprime))
-                    
-                for dx,dy in avg5_pix:
-                    xprime = x+dx
-                    yprime = y+dy
-                    if xprime >= 0 and xprime < im.width and yprime >= 0 and yprime < im.height:
-                        n5 += 1
-                        sum5 += im.getpixel((xprime,yprime))
-                                
-                
-                avg3 = np.divide(sum3,n3)
-                avg5 = np.divide(sum5,n5)
-
-                for cval in xrange(n_bands):
-                    if val_minus_l2s[cval] >= 0:
-                        x_vectors[cval].push_back(x)
-                        y_vectors[cval].push_back(y)
-                        val_vectors[cval].push_back(val[cval])
-                        avg3_vectors[cval].push_back(avg3[cval])
-                        avg5_vectors[cval].push_back(avg5[cval])
-                        l2s_vectors[cval].push_back(s_grid[x,y,cval]+l2diff[cval])
-               
-            im.close()       
-            if not l1s_satisfied: continue
-
-        # using numpy methods    
-        else:
-            imarray = np.array(im, dtype=int).transpose(1,0,2)
-            im.close()
-
-            # enforce L1S
-            if np.count_nonzero(imarray>=s_grid+l1diff) == 0: continue
-
-            avg3_array = [convolve2d(imarray[:,:,cval], avg3_kernel, mode='same', boundary='symm') for cval in xrange(n_bands)]
-            avg5_array = [convolve2d(imarray[:,:,cval], avg5_kernel, mode='same', boundary='symm') for cval in xrange(n_bands)]
+        avg3_array = [convolve2d(imarray[:,:,cval], avg3_kernel, mode='same', boundary='symm') for cval in xrange(n_bands)]
+        avg5_array = [convolve2d(imarray[:,:,cval], avg5_kernel, mode='same', boundary='symm') for cval in xrange(n_bands)]
 
             
-            for x,y,cval in np.argwhere(imarray>=s_grid+l2diff):
-                x_vectors[cval].push_back(x)
-                y_vectors[cval].push_back(y)
-                val_vectors[cval].push_back(imarray[x,y,cval])
-                avg3_vectors[cval].push_back(avg3_array[cval][x,y])
-                avg5_vectors[cval].push_back(avg5_array[cval][x,y])
-                l2s_vectors[cval].push_back(s_grid[x,y,cval]+l2diff[cval])
+        for y,x,cval in np.argwhere(imarray>=s_grid+l2diff):
+            x_vectors[cval].push_back(x)
+            y_vectors[cval].push_back(y)
+            val_vectors[cval].push_back(imarray[y,x,cval])
+            avg3_vectors[cval].push_back(avg3_array[cval][y,x])
+            avg5_vectors[cval].push_back(avg5_array[cval][y,x])
+            l2s_vectors[cval].push_back(s_grid[y,x,cval]+l2diff[cval])
                 
 
         # fill TTree with image data for each band
-        for cval, c in enumerate(im.mode):
+        for cval, c in enumerate(imarray.bands):
             
             color = np.array(c+'\0')
             t.SetBranchAddress('col', color)
@@ -291,9 +211,9 @@ if __name__ == '__main__':
 
     parser.add_argument("--border", type=int, default=0, help='Number of pixels around edge to crop out')
     parser.add_argument("--max_img", type=int, help='Maximum number of images to convert')
+    parser.add_argument("--source_format", action='store_true', help='Preserves data from files of format "phone_time_s.jpg"')
     parser.add_argument("--show", action='store_true', help='Generate graphs of background thresholds and saved pixels')
-    parser.add_argument("--numpy", action='store_true', help='Apply thresholds with numpy arrays.  Useful for large threshold gradients')
-
+    
     args = parser.parse_args()
 
     images = list(args.infiles)
@@ -302,7 +222,7 @@ if __name__ == '__main__':
         
     ti = time.clock()
     t = convert_to_root(images, args.l1, args.l2auto, args.l2manual, args.l2plus, args.sauto, args.smanual, \
-                        args.border, args.max_img, args.numpy)
+                        args.border, args.max_img)
 
     tf = time.clock()
       
@@ -315,7 +235,7 @@ if __name__ == '__main__':
         print "Drawing saved pixels..."
         c1 = r.TCanvas('c1','Saved Pixels',300,250*n_bands)
         c1.Divide(1,n_bands,0,0)
-        for cval,c in enumerate(mode):
+        for cval,c in enumerate(bands):
             c1.cd(cval+1)
             t.Draw('pix_y:pix_x','col=="%s"' % c, 'colz')
 
