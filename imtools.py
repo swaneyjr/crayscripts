8c84d2dd2c09a1834c402e8b3bcbfabbd775daab
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-10-20 02:49:23 -0700

    Update imtools.py

--- imtools.py
+++ imtools.py
@@ -6,13 +6,6 @@ import gzip
 
 class Imarray(np.array):
 
-# adds a vector TBranch to a TTree
-def vbranch(t, bname, btype=float):
-    btype_name = {float: 'double', int: 'int'}[btype]
-    v = r.vector(btype_name)()
-    setattr(t, '_%s'%bname, v)
-    t.Branch(bname, v)
-
 # returns an array of counts of ADC values in a region
 def find_spectrum(imarray, counts=1024, region=(0,0)+imarray.shape):
   return np.bincount(imarray[region[0]:region[2],region[1]:region[3]], minlength=counts)
