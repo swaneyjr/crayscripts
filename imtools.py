794bb8cd5c8dc2d2b129a064e9154f6b9e854383
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-10-20 13:57:25 -0700

    Update imtools.py

--- imtools.py
+++ imtools.py
@@ -4,17 +4,30 @@ from PIL import Image
 from scipy.signal import convolve2d
 import gzip
 
-class Imarray(np.array):
+class ImGrid(np.array):
+  def __new__(cls, input_array, bands=None):
+    obj = np.asarray(input_array).view(cls)
+    obj.bands = ###
+    return obj
+
+  def __array_finalize__(self, obj):
+    if obj is None: return
+    self.info = getattr(obj, 'info', None)
 
 # returns an array of counts of ADC values in a region
-def find_spectrum(imarray, counts=1024, region=(0,0)+imarray.shape):
+def spectrum(imarray, counts=1024, region=(0,0)+imarray.shape):
   return np.bincount(imarray[region[0]:region[2],region[1]:region[3]], minlength=counts)
 
 # opens an image file and returns an array of dimensions (h,w,band)
-def get_imarray(file_name, israw=False):
+def get_imarray(file_name):
   
-  if not raw:
+  raw_types = ['dng']
+  
+  # PIL
+  if not file_name.split('.')[1] in raw_types:
     return np.array(Image.open(file_name))
+  
+  # rawpy
   elif file_name.endswith('.gz'):
     f = gzip.open(file_name)
     file_name = file_name[:-3]
