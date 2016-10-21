ed77540034e56c51911b319a9c1ba13c0221ff96
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-10-20 17:17:22 -0700

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -5,31 +5,45 @@ import matplotlib.pyplot as plt
 matplotlib.use('tkagg')
 plt.ion()
 
-def plot_spectrum(images, rg, average):
+# make plots for each color band of distribution of ADC 
+def plot_spectrum(images, rg, normalize):
 
   adc_counts = 0
   
   for i, im in enumerate(images):
   
-    print
-    
    
     imarray = imtools.ImGrid(im)
     adc_counts += imtools.find_spectrum(imarray, region=rg)
     
-  plt.figure(1)
-  plt.hist(np.arange(counts), weights=adc_counts)
-  plt.xlabel('ADC Counts')
-  plt.ylabel('Frequency')
+  if normalize:
+    adc_counts.astype(float) /= adc_counts.sum()
+    
+  max_count = max(adc_counts[0].nonzero())
+  if max_count < 256:
+    max_count = 256
+    adc_counts = adc_counts[:,256]
+  else:
+    max_count = 1024
+    
+  for cval in xrange(adc_counts.shape[0])
+    plt.figure(cval)
+    plt.hist(np.arange(max_count), weights=adc_counts)
+    plt.xlabel('ADC Counts')
+    plt.ylabel('Frequency')
   plt.show()
     
 
 if __name__ == '__main__':
   from argparse import ArgumentParser
-  parser = ArgumentParser(description='')
-  parser.add_argument("--in", dest='images', required=True, nargs='+', help='')
-  parser.add_argument("--region, help='')
-  parser.add_argument("--average", help='')
+  parser = ArgumentParser(description='Generates histograms of ADC counts of images by color band')
+  parser.add_argument("--in", dest='images', required=True, nargs='+', help='Images from which spectrum is to be constructed')
+  parser.add_argument("--region", metavar="X0,Y0,X1,Y1", help='A comma delimited list of the region to compute spectrum across images')
+  parser.add_argument("--normalize", action='store_true', help='Normalize data')
   args = parser.parse_args()
-                      
+  
+  region = tuple(map(int, args.region.split(',')))
+  
+  plot_spectrum(args.images, region, args.normalize)
+  
   
