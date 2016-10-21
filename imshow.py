b5c96952dec3131cd99bfcbff791a78e3d9de788
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-10-21 13:53:36 -0700

    Update imshow.py

--- imshow.py
+++ imshow.py
@@ -4,6 +4,7 @@ from PIL import Image
 import numpy as np
 from scipy.signal import convolve2d
 from bg_threshold import outlier_cutoff
+import imtools
 
 # enable X-forwarding
 matplotlib.use('tkagg')
@@ -11,9 +12,6 @@ matplotlib.use('tkagg')
 from argparse import ArgumentParser
 parser = ArgumentParser(description = 'Make a colormap out of an image')
 parser.add_argument('img', help='Image to be displayed')
-parser.add_argument('-r', "--red", action='store_true', help='Draw R channel')
-parser.add_argument('-g', "--green", action='store_true', help='Draw G channel')
-parser.add_argument('-b', "--blue", action='store_true', help='Draw B channel')
 bg_option = parser.add_mutually_exclusive_group()
 bg_option.add_argument('-m','--minus_bg', action='store_true', help='Subtract avg7 values at each pixel')
 bg_option.add_argument('-d','--div_bg', action='store_true', help='Divide by avg7 values at each pixel.')
@@ -32,7 +30,7 @@ def plotchannel(imarray, cval):
     plt.imshow(imarray[:,:,cval], cmap='plasma', interpolation='nearest',vmin=0, vmax=mx)
     plt.colorbar()
     
-imarray = np.array(Image.open(args.img)).astype(int)
+imarray = ImGrid(args.img)
 sample_block = 4
 
 if args.minus_bg or args.div_bg:
@@ -48,19 +46,10 @@ if args.sample:
     imarray = np.repeat(np.repeat(imarray, sample_block, axis=0), sample_block, axis=1)
 
     
-n_fig = 1
-
-if args.red:
-    plt.figure(n_fig)
-    plotchannel(imarray, 0)
-    n_fig += 1
-if args.green:
-    plt.figure(n_fig)
-    plotchannel(imarray, 1)
-    n_fig += 1
-if args.blue:
-    plt.figure(n_fig)
-    plotchannel(imarray, 2)
+for cval,b in enumerate(imarray.bands)
+    plt.figure(i+1)
+    plotchannel(imarray, cval)
+    plt.title(b)
 
 plt.show()
 
