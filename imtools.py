b31d271a4ad70dde4cd14b00f9072c4fd52917a2
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-11-01 14:25:23 -0700

    Update imtools.py

--- imtools.py
+++ imtools.py
@@ -5,10 +5,11 @@ from scipy.signal import convolve2d
 import gzip
 
 class ImGrid(np.ndarray):
+  
+  compressed_types = ['jpg','png','gif']
+  zip_types = ['gz']
+  
   def __new__(cls, file_name, bands=None):
-    
-    raw_types = ['dng']
-    zip_types = ['gz']
     
     extensions = file_name.split('.')
   
@@ -19,9 +20,9 @@ class ImGrid(np.ndarray):
     else:
       f = open(file_name)
     
-    if extensions[-1] in raw_types:
+    if extensions[-1] not in compressed_types:
       raw = rawpy.imread(f)
-      imarray = np.array([raw.raw_image.copy()]).transpose(1,2,0).astype(int)
+      imarray = np.array([raw.raw_image_visible.copy()]).transpose(1,2,0).astype(int)
       bands = ['RAW']
       raw.close()
     
@@ -40,12 +41,50 @@ class ImGrid(np.ndarray):
     obj.height = imarray.shape[0]
     obj.width = imarray.shape[1]
     return obj
+  
+  def __new__(cls, fname1, fname2, bands=None):
+    
+    if set(fname1.split('.')) & set(compressed_types):
+      im_name = f1
+      raw_name = f2
+    else:
+      im_name = f2
+      raw_name = f1
+  
+    # open file
+    if raw_name.split('.')[-1] in zip_types:
+      raw_file = gzip.open(raw_name)
+      extensions = extensions[:-1]
+    else:
+      raw_file = open(raw_name)
+    
+    raw = rawpy.imread(raw_file)
+    rawarray = np.array([raw.raw_image_visible.copy()]).astype(int)
+    raw.close()
+    raw_file.close()
+    
+    # PIL
+    with Image.open(im_name) as im:
+      imarray = np.array(im).astype(int)
+      bands = ['RAW'] + list(im.mode)
+      if len(bands) == 1:
+        imarray = np.array([imarray])
+    
+    fullarray = np.array([r for r in rawarray] + [c for c in imarray]).transpose(1,2,0)
+    
+    obj = np.asarray(fullarray).view(cls)
+    obj.bands = bands
+    obj.height = fullarray.shape[0]
+    obj.width = fullarray.shape[1]
+    return obj
+
 
   def __array_finalize__(self, obj):
     if obj is None: return
     self.bands = getattr(obj, 'bands', None)
     self.height = getattr(obj, 'height', None)
     self.width = getattr(obj, 'width', None)
+
 
 # returns an array of counts of ADC values in a region
 def spectrum(imarray, counts=1024, region=None):
