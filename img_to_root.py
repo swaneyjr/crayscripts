00670327ca7fa6e5f38c6e36eb779ba909740994
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-10-31 20:44:03 -0700

    Update img_to_root.py

--- img_to_root.py
+++ img_to_root.py
@@ -213,7 +213,7 @@ if __name__ == '__main__':
     if args.show:
         
         print "Drawing saved pixels..."
-        c1 = r.TCanvas('c1','Saved Pixels',300,250*n_bands)
+        c1 = r.TCanvas('c1','Saved Pixels',300,250*len(bands))
         c1.Divide(1,n_bands,0,0)
         for cval,c in enumerate(bands):
             c1.cd(cval+1)
