6d8c0c4a0d26a008f76e2476833662525018293b
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-11-01 16:28:12 -0700

    Update imtools.py

--- imtools.py
+++ imtools.py
@@ -79,7 +79,7 @@ class ImGrid(np.ndarray):
     else:
       imarray = []
     
-    fullarray = np.array([r for r in rawarray] + [c for c in imarray]).transpose(1,2,0)
+    fullarray = np.array([r for r in rawarray] + [c for c in imarray])
     
     obj = np.asarray(fullarray).view(cls)
     obj.bands = bands
