03424451d1d1a5df51633034857522cfb4d8c7f6
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-11-18 01:44:38 -0800

    Update clean_tracks.py

--- clean_tracks.py
+++ clean_tracks.py
@@ -83,56 +83,27 @@ def extract_tracks(pixels, min_pix_tr, iso_thresh):
 # calculates the angle of a track via hough transform
 def find_hough_theta(pixels):
 
-    weight = False
-
     n = len(pixels)
+    
+    # orthogonal least squares fit
+    sum_x = sum_y = sum_x_sq = sum_y_sq = sum_xy = 0
+    for p in pixels:
+        sum_x += p.x
+        sum_y += p.y
+        sum_x_sq += p.x**2
+        sum_y_sq += p.y**2
+        sum_xy += p.x*p.y
 
-    if weight:
-        intersections = []
-
-        def theta_weight(dx, dy):
-            return dx**2 + dy**2
-        
-        for i,p1 in enumerate(pixels):
-            for p2 in pixels[i+1:]:
-                dx = p1.x-p2.x
-                dy = p1.y-p2.y
-                weight = theta_weight(dx, dy)         
-                if dy == 0:
-                    intersections.append((math.pi/2, weight))
-                else:
-                    intersections.append((math.atan(-dx/dy), weight))
-
-        # take weighted average
-        theta_weighted_sum = 0
-        for th, weight in intersections:
-            theta_weighted_sum += cmath.rect(weight, 2*th)
-        theta = cmath.phase(theta_weighted_sum)/2
-        rho = 0
-        for p in pixels:
-            rho += p.x*math.cos(theta) + p.y*math.sin(theta)
-        rho /= n
-
-    else:
-        # orthogonal least squares fit
-        sum_x = sum_y = sum_x_sq = sum_y_sq = sum_xy = 0
-        for p in pixels:
-            sum_x += p.x
-            sum_y += p.y
-            sum_x_sq += p.x**2
-            sum_y_sq += p.y**2
-            sum_xy += p.x*p.y
-
-        num = n*sum_xy-sum_x*sum_y
-        den = n*sum_x_sq-n*sum_y_sq-sum_x**2+sum_y**2
-        if den == 0:
-            if num == 0:
-                theta = float('nan')
-            else:
-                theta = math.copysign(math.pi/4., -num)
+    num = n*sum_xy-sum_x*sum_y
+    den = n*sum_x_sq-n*sum_y_sq-sum_x**2+sum_y**2
+    if den == 0:
+        if num == 0:
+            theta = float('nan')
         else:
-            theta = 0.5 * math.atan2(-2*num, -den)
-        rho = (sum_x*math.cos(theta)+sum_y*math.sin(theta))/n
+            theta = math.copysign(math.pi/4., -num)
+    else:
+        theta = 0.5 * math.atan2(-2*num, -den)
+    rho = (sum_x*math.cos(theta)+sum_y*math.sin(theta))/n
 
     # calculate other stats based on theta fit
 
@@ -207,6 +178,7 @@ def clean_tracks(t0, min_pix_tr, iso_thresh, fit):
 
         # read pixels from TTree
         raw_pixels = list(starmap(Pixel, izip(evt.pix_x, evt.pix_y, evt.pix_val, evt.pix_avg3, evt.pix_avg5)))
+        total_pix += len(raw_pixels)
         tracks = extract_tracks(raw_pixels, min_pix_tr, iso_thresh)
 
         
@@ -256,7 +228,6 @@ def clean_tracks(t0, min_pix_tr, iso_thresh, fit):
             pix_tr[0] = t2.pix_x.size()
             max_val[0] = max_pix_val
             
-            total_pix += len(raw_pixels)
             saved_pix += t2.pix_x.size()
 
             t2.Fill()
@@ -265,13 +236,11 @@ def clean_tracks(t0, min_pix_tr, iso_thresh, fit):
     print "Done!"
     print
     print "Input events:    %d" % total_events
-    print "Output events:   %d\t(%.2f%%)" % (t2.GetEntries(), 100.*t2.GetEntries()/total_events)
-    print "Pixels in raw tracks:  %d" % (total_pix)
+    print "Output events:   %d" % t2.GetEntries()
+    print "Pixels in raw tracks:  %d" % total_pix
     print "Pixels saved:    %d\t(%.2f%%)" % (saved_pix, 100.*saved_pix/total_pix)
 
     return t2
-
-
 
 
 if __name__ == '__main__':
@@ -280,7 +249,7 @@ if __name__ == '__main__':
     parser.add_argument("--in", required=True, dest='infiles', nargs='+', help='File to be cleaned')
     parser.add_argument("--out", default='tracks.root', help='Output file name')
     parser.add_argument("--min", type=int, default=2, help='Minimum pix_tr to be kept')
-    parser.add_argument("--iso", type=int, default=15, help='Distance above which a pixel is classified as isolated')
+    parser.add_argument("--iso", type=int, default=5, help='Distance above which a pixel is classified as isolated')
     parser.add_argument("--fit", action='store_true', help='Compute linear fit parameters')
     args = parser.parse_args()
 
