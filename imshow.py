ee1731bd6b9f0e670f78dea948a0d6d531a597dd
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-11-21 11:36:58 -0800

    Update imshow.py

--- imshow.py
+++ imshow.py
@@ -14,7 +14,7 @@ def plotchannel(imarray, cval, minus_bg=False, div_bg=False, colormap='plasma'):
     elif div_bg:
         mx = 5
     else:
-        mx = np.amax(imtools.outlier_cutoff(imarray))
+        mx = imtools.outlier_cutoff(imarray)[cval]
         mx += 5 - (mx%5)
     plt.imshow(imarray[cval], cmap=colormap, interpolation='nearest',vmin=0, vmax=mx)
     plt.colorbar()
