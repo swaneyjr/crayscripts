5efcf30fb99b39139abd1c0b6afbb5bbd74e1065
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-11-20 22:02:25 -0800

    Update track_tools.py

--- track_tools.py
+++ track_tools.py
@@ -1,14 +1,17 @@
 import ROOT as r
 import math
 
-def compare_theta(t, name='hist', n_bins=100, cut=''):
+def compare_theta(t, name='hist', n_bins=100, cut=None):
 
   hist = r.TH1F(name,'Track angle excess, compared to 90 degree rotation',n_bins,-math.pi/2,math.pi/2)
-  hist.GetXAxis().SetTitle('Track angle')
-  hist.GetYAxis().SetTitle('Standard deviations above perpendicular')
+  hist.GetXaxis().SetTitle('Track angle')
+  hist.GetYaxis().SetTitle('Standard deviations above perpendicular')
   r_hist = r.TH1F('r_hist','r_hist',n_bins,-math.pi/2,math.pi/2)
   std_hist = r.TH1F('std_hist','std_hist',n_bins,-math.pi/2,math.pi/2)
   
+  if cut:
+    r.gROOT.cd()
+    t = t.CopyTree(cut)
   for track in t:
     hist.Fill(track.theta)
     if track.theta<0:
