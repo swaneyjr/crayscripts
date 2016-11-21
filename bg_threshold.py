d8aa08f2f3ab2ad09a006b1a786f2179073c3343
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2016-11-21 12:22:46 -0800

    Update bg_threshold.py

--- bg_threshold.py
+++ bg_threshold.py
@@ -88,9 +88,9 @@ def find_bg(images, out, conv_len=5, bg_cutoff=True, max_img=None):
             if (i+1) % 10 == 0:
                 print " %d/%d" % (i+1,n_img_bg)
             im_grid = imtools.ImGrid(im)
-            print "Max pixel = %d" % np.amax(im_grid)
             s_grid = np.median([max_grid, s_grid, im_grid], axis=0).astype(int)
             max_grid = np.maximum(max_grid, s_grid, im_grid).astype(int)
+        print "Max pixel = %d" % np.amax(max_grid)
 
     print "Downsampling image..."
 
