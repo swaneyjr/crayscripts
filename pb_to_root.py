19f2dd58893c4977d24342cdb262027759c1b6c1
swaneyjr <jswaney@uci.edu>
2017-01-08 19:22:35 -0800

    Bug fix

--- pb_to_root.py
+++ pb_to_root.py
@@ -27,8 +27,8 @@ def pb_to_trees(fname):
     exposure = r.TTree('exposure', 'Exposure from protobuf data')
     events = r.TTree('events', 'Events from protobuf data')
 
-    str_type = {int:'/I', long:'/L', float:'/D', bool:'/O', str:'/C'}
-    full_type = {int:'int', long:'long', float:'double', bool:'bool',str:'char'}
+    short_type = {int:'/i', long:'/l',float:'/D',bool:'/O',str:'/C'}
+    full_type = {int:'int', long:'long',float:'double',bool:'bool',str:'char'}
 
     # assign appropriate branches to trees
 
@@ -40,13 +40,15 @@ def pb_to_trees(fname):
     for xb_field,val in dc.exposure_blocks[0].ListFields():
         if xb_field.label != 3: # save repeated fields for events
             xb_containers[xb_field.name]= np.zeros(1,dtype=type(val))
-            exposure.Branch(xb_field.name, xb_containers[xb_field.name])
+            exposure.Branch(xb_field.name, xb_containers[xb_field.name], \\
+                            xb_field.name+short_type[type(val)])
         
 
     for evt_field,val in dc.exposure_blocks[0].events[0].ListFields():
         if evt_field.label != 3: # save repeated fields for pixels
             evt_containers[evt_field.name] = np.zeros(1,dtype=type(val))
-            events.Branch(evt_field.name, evt_containers[evt_field.name])
+            events.Branch(evt_field.name, evt_containers[evt_field.name], \\
+                          evt_field.name+short_type[type(val)])
 
     for pix_field,val in dc.exposure_blocks[0].events[0].pixels[0].ListFields():
         pix_containers[pix_field.name] = r.vector(full_type[type(val)])()
