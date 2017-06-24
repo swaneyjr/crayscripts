31626a83dc85094074671058605f5ebf8a599252
swaneyjr <jswaney@uci.edu>
2017-06-24 14:04:33 -0700

    Added comparison of compressed and uncompressed weights

--- show_precal.py
+++ show_precal.py
@@ -1,7 +1,5 @@
 import crayfis_data_pb2 as pb
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 import cv2
 
 def get_precal(fname):
@@ -12,33 +10,68 @@ def get_precal(fname):
 
     return dc.precalibration_results[0]
 
-def get_weight_array(precal, size=None):
+def get_weight_array(precal, size=None, compressed=False):
    
     res_x = precal.sample_res_x
     res_y = precal.sample_res_y
-    downsample = np.array(precal.weights).reshape(res_y,res_x)
+    
+    if compressed:
+        compressed_array = np.array(precal.compressed_weights)
+        downsample,retval = cv2.imdecode(precal.compressed_format, compressed_array)
+    else:
+        downsample = np.array(precal.weights).reshape(res_y,res_x)
+    
     if size:
-        resize = cv2.INTER_CUBIC
         if precal.interpolation:
-            resize = precal.interpolation
-	return cv2.resize(downsample, size, interpolation=resize)
+            return cv2.resize(downsample, size, interpolation=precal.interpolation)
+        return cv2.resize(downsample, size)
     return downsample
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
+    import matplotlib
+    import matplotlib.pyplot as plt
+    
     parser = ArgumentParser()
     parser.add_argument('precal', help='protobuf .bin file')
     parser.add_argument('--resample', help='Comma-delimited size to rescale')
+    compression = parser.add_mutually_exclusive_group(required=True)
+    compression.add_argument('-u','--uncompressed', action='store_true', help='Use uncompressed weights')
+    compression.add_argument('-c','--compressed', action='store_true', help='Use compressed weights')
+    compression.add_argument('-x','--compare', action='store_true', help='Find ratio of compressed/uncompressed weights')
     args = parser.parse_args()
 
     if args.resample:
 	args.resample = tuple(map(int, args.resample.split(',')))
 
     matplotlib.use('tkagg')
-    plt.figure(1)
-    plt.imshow(get_weight_array(get_precal(args.precal), args.resample), cmap='plasma', interpolation='nearest')
-    plt.colorbar()
+    
+    if not args.compare:
+        plt.figure(1)
+        plt.imshow(get_weight_array(get_precal(args.precal), args.resample, args.compressed), cmap='plasma', interpolation='nearest')
+        plt.colorbar()
+    else:
+        precal = get_precal(args.precal)
+
+        compressed = get_weight_array(precal, args.resample, True)
+        uncompressed = get_weight_array(precal, args.resample, False)
+        
+        figure(1)
+        plt.title('Uncompressed')
+        plt.imshow(uncompressed, cmap='plasma', interpolation='nearest')
+        plt.colorbar()
+
+        figure(2)
+        plt.title('Compressed')
+        plt.imshow(uncompressed, cmap='plasma', interpolaion='nearest')
+        plt.colorbar()
+
+        figure(3)
+        plt.title('Compressed/Uncompressed')
+        plt.imshow(compressed/uncompressed, cmap='coolwarm', interpolation='nearest')
+        plt.colorbar()
+        
 
     plt.show()
     
