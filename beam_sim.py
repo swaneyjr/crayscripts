c389af2492f83d44bacd40b6679025d863717385
swaneyjr <jeffrey.r.swaney@vanderbilt.edu>
2019-04-15 23:32:21 -0700

    Added visualization

--- beam_sim.py
+++ beam_sim.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import time
+import uuid
 
 import numpy as np
 from scipy.sparse import coo_matrix, save_npz
@@ -34,64 +36,143 @@ def map_coords(xp, yp, phi_p, theta_p, zs, phi_s, theta_s, psi_s, xs0, ys0):
                             center of the sensor
     """
 
-    # convert to polar coords
+    # convert particle positions to polar coords
     rp = np.sqrt(xp**2 + yp**2)
     xi_p = np.arctan2(yp, xp)
 
     # calculate trig functions
-    cos_xi = np.cos(xi_p - phi_s)
-    sin_xi = np.sin(xi_p -phi_s)
+    cos_xi = np.cos(xi_p - phi_s.reshape(-1,1))
+    sin_xi = np.sin(xi_p -phi_s.reshape(-1,1))
 
-    cos_phi = np.cos(phi_p - phi_s)
-    sin_phi = np.sin(phi_p - phi_s)
+    cos_phi = np.cos(phi_p - phi_s.reshape(-1,1))
+    sin_phi = np.sin(phi_p - phi_s.reshape(-1,1))
 
-    cos_psi = np.cos(psi_s)
-    sin_psi = np.sin(psi_s)
+    cos_psi = np.cos(psi_s).reshape(-1,1)
+    sin_psi = np.sin(psi_s).reshape(-1,1)
 
-    sec_theta = 1 / np.cos(theta_s)
-    tan_theta = np.tan(theta_s)
+    sec_theta = 1 / np.cos(theta_s).reshape(-1,1)
+    tan_theta = np.tan(theta_s).reshape(-1,1)
     
     denom = 1 / np.tan(theta_p) + tan_theta * cos_phi
 
-    zt = zs - rp * cos_xi * tan_theta
+    zt = zs.reshape(-1,1) - rp * cos_xi * tan_theta
+    xr = -xs0.reshape(-1,1)
+    yr = -ys0.reshape(-1,1)
 
     # result
-    xs = -xs0 + rp * (cos_xi *cos_psi*sec_theta + sin_xi *sin_psi) \
-              + zs * (cos_phi*cos_psi*sec_theta + sin_phi*sin_psi) / denom
-    ys = -ys0 + rp * (sin_xi *cos_psi - cos_xi *sec_theta*sin_psi) \
-              + zs * (sin_phi*cos_psi - cos_phi*sec_theta*sin_psi) / denom
+    xs = -xr + rp * (cos_xi *cos_psi*sec_theta + sin_xi *sin_psi) \
+             + zt * (cos_phi*cos_psi*sec_theta + sin_phi*sin_psi) / denom
+    ys = -yr + rp * (sin_xi *cos_psi - cos_xi *sec_theta*sin_psi) \
+             + zt * (sin_phi*cos_psi - cos_phi*sec_theta*sin_psi) / denom
 
     return xs, ys
+
+
+def visualize(spot_size, n_particles, x_s, y_s, phi_s, theta_s, psi_s):
+
+    global RES_X, RES_Y, PIX_SIZE
+
+    import matplotlib.pyplot as plt
+    from matplotlib.patches import Polygon
+    from matplotlib.collections import PatchCollection
+
+    fig = plt.figure()
+    ax = fig.gca()
+
+    # first draw beam profile
+    beam_xy = multivariate_normal(mean=[0,0], cov=args.spot_size/2)
+    sz = 1.5*spot_size
+    x = np.linspace(-sz/2, sz/2, 100)
+    y = np.linspace(-sz/2, sz/2, 100)
+    pos = np.dstack(np.meshgrid(x,y))
+    intensity = n_particles*beam_xy.pdf(pos)
+    plt.imshow(intensity, extent=2*[-sz/2, sz/2], cmap='plasma')
+    plt.colorbar()
+
+    # next add rectangles for each phone
+    n_phones = x_s.size
+    
+    lim_x = RES_X * PIX_SIZE / 2
+    lim_y = RES_Y * PIX_SIZE / 2
+    rects = np.repeat(np.array([[
+            [-lim_x, -lim_y], 
+            [lim_x, -lim_y], 
+            [lim_x, lim_y],
+            [-lim_x, lim_y]]]), n_phones, axis=0)
+
+    # apply translation
+    rects += np.dstack([x_s, y_s]).reshape(n_phones,1,2)
+
+    # do rotations
+
+    cos_phi = np.cos(phi_s)
+    sin_phi = np.sin(phi_s)
+    phi_mat = np.array([[cos_phi, -sin_phi], [sin_phi, cos_phi]])
+
+    theta_mat = np.array([
+        [np.cos(theta_s), np.zeros(n_phones)],
+        [np.zeros(n_phones), np.ones(n_phones)]
+        ])
+
+    cos_psi = np.cos(psi_s)
+    sin_psi = np.sin(psi_s)
+    psi_mat = np.array([[cos_psi, -sin_psi], [sin_psi, cos_psi]])
+
+
+    # FIXME: there's a more elegant way to do this with einsum
+
+    for iphone in range(args.n_phones):
+         
+        for corner in range(4):
+            psi_rot = np.matmul(psi_mat[:,:,iphone], rects[iphone][corner])
+            theta_rot = np.matmul(theta_mat[:,:,iphone], psi_rot)
+            phi_rot = np.matmul(phi_mat[:,:,iphone], theta_rot)
+            rects[iphone][corner] = phi_rot
+
+
+    polygons = [Polygon(rects[iphone]) for iphone in range(n_phones)]
+    p = PatchCollection(polygons, edgecolors='r', facecolors='none', linewidths=1)
+    ax.add_collection(p)
+
+    plt.show()
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser(description='')
-    parser.add_argument('--out', default='beam_sim.npz')
+    parser.add_argument('--out', default='beam_sim.npz', help='Base file name, with a .npz extension')
 
-    parser.add_argument('--n_phones', default=3, type=int)
-    parser.add_argument('--fps', default=1., type=float)
-    parser.add_argument('--phone_gap', default=10., type=float)
-    parser.add_argument('--eff', default=1., type=float)
+    parser.add_argument('--n_phones', default=3, type=int, help='Number of phones')
+    parser.add_argument('--fps', default=1., type=float, help='Frame rate')
+    parser.add_argument('--phone_gap', default=10., type=float, help='Average distance between planes of phone sensors')
+    parser.add_argument('--eff', default=1., type=float, help='Efficiency of phones')
 
-    parser.add_argument('--gap_stdev', default=1., type=float)
-    parser.add_argument('--theta_dev', default=.05, type=float)
-    parser.add_argument('--psi_dev', default=.05, type=float)
-    parser.add_argument('--xy_dev', default=1, type=float)
+    parser.add_argument('--gap_stdev', default=.05, type=float, help='Standard deviation applied to phone_gap')
+    parser.add_argument('--theta_dev', default=.05, type=float, help='Standard deviation in Eulerian theta for phone rotations')
+    parser.add_argument('--psi_dev', default=.05, type=float, help='Standard deviation of Eulerian psi (about -phi) for phone rotations')
+    parser.add_argument('--xy_dev', default=0.7, type=float, help='Standard deviation of phone positions in rotated plane with respect to apparatus')
+    parser.add_argument('--xy_offset', default=1.0, type=float, help='Standard deviation of phone apparatus position with respect to the beam')
 
-    parser.add_argument('--n_spills', default=1, type=int)
-    parser.add_argument('--spill_size', default=5e5, type=int)
-    parser.add_argument('--spot_size', default=6., type=float)
-    parser.add_argument('--collimation', default=.001, type=float)
+    parser.add_argument('--n_spills', default=1, type=int, help='Number of beam spills')
+    parser.add_argument('--spill_size', default=5e5, type=int, help='Number of particles in each beam spill')
+    parser.add_argument('--spot_size', default=6., type=float, help='Diameter of beam in mm')
+    parser.add_argument('--collimation', default=.001, type=float, help='Angular variation of beam particles in radians')
 
-    parser.add_argument('--visualization', action='store_true')
+    parser.add_argument('--visualize', action='store_true', help='Create a visualization of the phone positions')
     args = parser.parse_args()
 
-    fname = args.out.replace('.npz', '_p{}_t{}.npz')
+    basename = os.path.basename(args.out)
+    dirname = os.path.dirname(args.out)
+    
+    os.makedirs(os.path.join(dirname, 'raw'), exist_ok=True)
+    f_truth_name = basename.replace('.npz', '_truth.npz')
+    spill_template = basename.replace('.npz', '_p{}_t{}.npz')
     millis = int(time.time() * 1000)
 
     # create the phone geometries
-    n = (args.n_phones, 1)
+    n = args.n_phones
+
+    phone_hwid = [uuid.uuid4().hex[:16] for _ in range(n)]
 
     phone_z = np.arange(args.n_phones).reshape(n) * args.phone_gap \
             + np.random.normal(0, args.gap_stdev, n)
@@ -99,66 +180,17 @@ if __name__ == '__main__':
     phone_psi = -phone_phi + np.random.normal(0, args.psi_dev, n)
     phone_theta = np.abs(np.random.normal(0, args.theta_dev, n))
 
-    phone_x = np.random.normal(0, args.xy_dev, n)
-    phone_y = np.random.normal(0, args.xy_dev, n)
+    offset_xy = np.random.normal(0, args.xy_offset, 2)
+    phone_x = np.random.normal(offset_xy[0], args.xy_dev, n)
+    phone_y = np.random.normal(offset_xy[1], args.xy_dev, n)
 
-    beam_xy = multivariate_normal(mean=[0,0], cov=args.spot_size/2)
+    np.savez(os.path.join(dirname, f_truth_name), hwid=phone_hwid, x=phone_x, \
+            y=phone_y, phi=phone_phi, theta=phone_theta, psi=phone_psi)
 
-    if args.visualization:
-        import matplotlib.pyplot as plt
-        from matplotlib.patches import Polygon
-        from matplotlib.collections import PatchCollection
-
-        fig = plt.figure()
-        ax = fig.gca()
-
-        # first draw beam profile
-        sz = 1.5*args.spot_size
-        x = np.linspace(-sz/2, sz/2, 100)
-        y = np.linspace(-sz/2, sz/2, 100)
-        pos = np.dstack(np.meshgrid(x,y))
-        intensity = args.spill_size*beam_xy.pdf(pos)
-        plt.imshow(intensity, extent=2*[-sz/2, sz/2], cmap='plasma')
-        plt.colorbar()
-
-        # next add rectangles for each phone
-        lim_x = RES_X * PIX_SIZE / 2
-        lim_y = RES_Y * PIX_SIZE / 2
-        rects = np.repeat(np.array([[
-                [-lim_x, -lim_y], 
-                [lim_x, -lim_y], 
-                [lim_x, lim_y],
-                [-lim_x, lim_y]]]), args.n_phones, axis=0)
-
-        # apply translation
-        rects += np.dstack([phone_x, phone_y]).reshape(args.n_phones,1,2)
-
-        # do rotations
-        # FIXME: there's a more elegant way to do this
-        for iphone in range(args.n_phones):
-            cos_psi = np.cos(phone_psi[iphone,0])
-            sin_psi = np.sin(phone_psi[iphone,0])
-            psi_mat = np.array([[cos_psi, -sin_psi], [sin_psi, cos_psi]])
-
-            theta_mat = np.array([[np.cos(phone_theta[iphone,0]), 0],[0,1]])
-
-            cos_phi = np.cos(phone_phi[iphone,0])
-            sin_phi = np.sin(phone_phi[iphone,0])
-            phi_mat = np.array([[cos_phi, -sin_phi], [sin_phi, cos_phi]])
-
-            for corner in range(4):
-                psi_rot = np.matmul(psi_mat, rects[iphone][corner])
-                theta_rot = np.matmul(theta_mat, psi_rot)
-                phi_rot = np.matmul(phi_mat, theta_rot)
-                rects[iphone][corner] = phi_rot
-
-
-        polygons = [Polygon(rects[iphone]) for iphone in range(args.n_phones)]
-        p = PatchCollection(polygons, edgecolors='r', facecolors='none', linewidths=1)
-        ax.add_collection(p)
-
-        plt.show()
-
+    if args.visualize:
+        visualize(args.spot_size, args.spill_size, phone_x, phone_y, \
+                phone_phi, phone_theta, phone_psi)
+        
     # now create the beam spill
     for ispill in range(args.n_spills):
 
@@ -207,8 +239,9 @@ if __name__ == '__main__':
                         shape=(RES_Y, RES_X))
 
                 # save output 
-                t_abs = millis + int(1000*(BEAM_PERIOD*ispill + tmin))
-                save_npz(fname.format(iphone, t_abs), sparse_hits)
+                t_abs = millis + int(1000*(BEAM_PERIOD*ispill + tmax))
+                f_spill_name = spill_template.format(phone_hwid[iphone], t_abs)
+                save_npz(os.path.join(dirname, 'raw', f_spill_name), sparse_hits)
 
                 tmin = tmax
                 tmax += 1 / args.fps
